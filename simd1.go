6e9e9aa3535ef375d7bfe63aaa7066237e885aee
LosBunkos <nadavvadan@gmail.com>
2018-06-03 00:22:53 +0300

    go fmt

--- simd1.go
+++ simd1.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"gonum.org/v1/gonum/floats"
 	"gonum.org/v1/gonum/blas/blas64"
+	"gonum.org/v1/gonum/floats"
 	"math"
 	"math/rand"
 	"sync"
@@ -19,11 +19,13 @@ const workFactor = tasks / threads
 const totalDotProducts = runs * tasks
 
 func toHuman(val float64, base10 bool) string {
-	sufs := [5]string {"", "K", "M", "G", "T"}
+	sufs := [5]string{"", "K", "M", "G", "T"}
 	i := val
 	idx := 0
 	var base float64 = 1024
-	if base10 {base = 1000}
+	if base10 {
+		base = 1000
+	}
 
 	for ; i > base; i /= base {
 		idx++
@@ -75,7 +77,7 @@ func doSearch(candidate *blas64.Vector, container *[tasks]blas64.Vector, results
 func filterResults(results [tasks]float64) (matches []int) {
 	matches, err := floats.Find(matches, passesThreshold, results[:], -1)
 
-	if (err != nil) {
+	if err != nil {
 		panic(err)
 	}
 
@@ -85,9 +87,9 @@ func filterResults(results [tasks]float64) (matches []int) {
 func main() {
 
 	fmt.Println("Initializing vectors DB (this might take a while) for", toHuman(tasks, true), "vectors")
-	fmt.Println("Going to use", toHuman(8 * 256 * tasks, false)+"B", "Memory for DB")
-	fmt.Println("Going to use", toHuman(8 * tasks, false)+"B", "Memory for matches array")
-	fmt.Println("Total RAM not counting program and GC overheads:", toHuman(8 * (256 + 1) * tasks, false)+"B")
+	fmt.Println("Going to use", toHuman(8*256*tasks, false)+"B", "Memory for DB")
+	fmt.Println("Going to use", toHuman(8*tasks, false)+"B", "Memory for matches array")
+	fmt.Println("Total RAM not counting program and GC overheads:", toHuman(8*(256+1)*tasks, false)+"B")
 
 	if math.Mod(tasks, threads) != 0 {
 		panic("tasks % threads should be 0")
@@ -98,7 +100,7 @@ func main() {
 	initDB(container[:])
 	end := time.Now()
 	took := end.Sub(start)
-	fmt.Println("Initialized DB (took", took, "total and", took / tasks, "per vector)")
+	fmt.Println("Initialized DB (took", took, "total and", took/tasks, "per vector)")
 	fmt.Println()
 	fmt.Printf("Running %s runs of %s tasks (total %s dot products) using %d threads\n",
 		toHuman(runs, true), toHuman(tasks, true), toHuman(totalDotProducts, true), threads)
@@ -112,12 +114,13 @@ func main() {
 	var totalMatches int64
 
 	for i := 0; i < runs; i++ {
-		if i % 10 == 0 {
+		if i%10 == 0 {
 			fmt.Printf("Running iter #%d\r", i)
 		}
 		if i+1 == runs {
 			fmt.Print("<----- Done ----->")
 		}
+
 		// Measure searching
 		temp := time.Now()
 		doSearch(&candidate, &container, &results)
@@ -144,5 +147,5 @@ func main() {
 	fmt.Println("Took ~", totalTook/runs, "per run")
 	fmt.Println()
 	fmt.Println("Total time elapsed:", totalTook)
-	fmt.Println("Total time per vector in DB:", totalTook / totalDotProducts)
+	fmt.Println("Total time per vector in DB:", totalTook/totalDotProducts)
 }
