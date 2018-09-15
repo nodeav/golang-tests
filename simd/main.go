a22890f5123f29dbf6a42749a669c008539a14f6
LosBunkos <nadavvadan@gmail.com>
2018-09-15 21:44:59 +0300

    better formatting and optional cpu profiling

--- simd/main.go
+++ simd/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gonum.org/v1/gonum/blas/blas64"
+	"log"
+	"os"
+	"os/exec"
+	"runtime/pprof"
 	DB "simd/simd/search"
 	"simd/simd/storage"
 	"time"
-	"flag"
-	"os/exec"
 )
+
+func toHuman(val float64, base10 bool) string {
+	sufs := [5]string{"", "K", "M", "G", "T"}
+	i := val
+	idx := 0
+	var base float64 = 1024
+	if base10 {
+		base = 1000
+	}
+
+	for ; i > base; i /= base {
+		idx++
+	}
+	return fmt.Sprintf("%.2f%s", i, sufs[idx])
+}
 
 func purge() {
 	purgeStart := time.Now()
@@ -57,22 +75,38 @@ func loadDB(dbPath string) {
 		}
 	}
 	fmt.Printf("Found [ %d ] entries of which [ %d ] were invalid\n", len(db)-numNoInc, numNoInc)
+	vecsPerSecond := int64(len(db)) / int64(loadTook/time.Second)
+	fmt.Printf("Vecs per second: %s/s\n", toHuman(float64(vecsPerSecond), false))
+	fmt.Printf("Read speed: %sB/s\n", toHuman(float64(vecsPerSecond*storage.VectSizeOnDisk), false))
 }
 
 func main() {
 	var load, save bool
-	var dbPath string
+	var dbPath, cpuprofilePath string
 	var dbSize int
 
 	flag.BoolVar(&load, "load", true, "Benchmark loading the db")
 	flag.BoolVar(&save, "save", true, "Benchmark saving the db")
-	flag.StringVar(&dbPath,"db", "./db", "Path to load/save the db. (Should exist)")
+	flag.StringVar(&dbPath, "db", "./db", "Path to load/save the db. (Should exist)")
 	flag.IntVar(&dbSize, "size", 5e5, "Amount of db entries")
+	flag.StringVar(&cpuprofilePath, "cpuprofile", "./cpu-prof", "write cpu profile to `file`")
+
 	flag.Parse()
 
-	flag.VisitAll(func (f *flag.Flag) {
+	flag.VisitAll(func(f *flag.Flag) {
 		fmt.Println("Key", f.Name, "Value", f.Value)
 	})
+
+	if cpuprofilePath != "" {
+		f, err := os.Create(cpuprofilePath)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 
 	if save {
 		fmt.Println("save is", save)
